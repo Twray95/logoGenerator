fcfa784a7f20417eb6ee4bd6ffac44d7cb037766
Thomas Wray <twray95@gmail.com>
2023-03-07 15:06:33 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+//inquirer prompts
+//3 letters max
+//letter color
+//shape (circle, triangle, square)
+//shape color
+//generates
